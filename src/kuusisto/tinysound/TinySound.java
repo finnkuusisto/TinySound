aebc265e97f97698a26781727a89113795255b08
Finn Kuusisto <finn.kuusisto@gmail.com>
2013-11-17 18:19:04 -0600

    Updated version number in TinySound class.

--- src/kuusisto/tinysound/TinySound.java
+++ src/kuusisto/tinysound/TinySound.java
@@ -63,7 +63,7 @@ import kuusisto.tinysound.internal.UpdateRunner;
  */
 public class TinySound {
 	
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 
 	/**
 	 * The internal format used by TinySound.
